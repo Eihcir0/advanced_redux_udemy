bacc4e1d68c869819240ee580b1fe5267ea6e39d
Eihcir0 <eihcir0@gmail.com>
2017-01-22 18:10:21 -0800

    completed server

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,7 +7,10 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
-
+exports.signin = function(req, res, next) {
+  //correct pw verified - just need token:
+  res.send({ token: tokenForUser(req.user)})
+}
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
